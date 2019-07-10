2cbbbd49f1bb855ceb95bfabf13eac626a2b0d9d
jz4o <26317360+jz4o@users.noreply.github.com>
2019-07-10 22:57:52 +0900

    指定したシートが存在しない場合に作成する処理を実装

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function myFunction() {
-  
+var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+
+/**
+ * 受取った引数をもとにシートに行を追加.
+ *
+ * @param hash json シートに登録する内容
+ *   {
+ *     'sheet': '<sheetName>',
+ *     'data' : {
+ *       '<columnName1>': '<value1>',
+ *       '<columnName2>': '<value2>',
+ *       ...
+ *       '<columnNameN>': '<valueN>'
+ *     }
+ *   }
+ */
+function marshall(json) {
+  var sheet = spreadSheet.getSheetByName(json['sheet']);
+  if (sheet == null) {
+    sheet = spreadSheet.insertSheet(json['sheet']);
+  }
 }
