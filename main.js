ed0ce7b06b0e83bcab9b69bb3d3415d9e2a937ba
jz4o <26317360+jz4o@users.noreply.github.com>
2019-07-11 23:30:51 +0900

    指定したカラムが存在しない場合に作成する処理を実装

--- main.js
+++ main.js
@@ -19,4 +19,16 @@ function marshall(json) {
   if (sheet == null) {
     sheet = spreadSheet.insertSheet(json['sheet']);
   }
+
+  var column_names = sheet.getLastColumn() > 0 ? sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues().shift() : [];
+  var add_column_names = [];
+  for (column in json['data']) {
+    if (column_names.indexOf(column) < 0) {
+      add_column_names.push(column);
+    }
+  }
+  if (add_column_names.length > 0) {
+    sheet.getRange(1, column_names.length + 1, 1, add_column_names.length).setValues([add_column_names]);
+    column_names = column_names.concat(add_column_names);
+  }
 }
